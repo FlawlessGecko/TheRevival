c98179dfd98c712815c0ed0b934c92757b46ef1a
FlawlessGecko <hops.ale@gmail.com>
2014-09-17 21:18:26 -0700

    Delete Blockstoneybrick.java

--- src/main/java/block/Blockstoneybrick.java
+++
@@ -1,9 +0,0 @@
-package block;
-
-public class Blockstoneybrick extends BlockTR {
-public Blockstoneybrick(){
-	super();
-	this.setBlockName("blockstoneybrick");
-	this.setBlockTextureName("blockstoneybrick");
-}
-}
